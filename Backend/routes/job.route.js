790ef6800e819912687d59b111ea46a70dcf33fe
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-11-24 01:51:50 +0530

    live

--- Backend/routes/job.route.js
+++ Backend/routes/job.route.js
@@ -4,7 +4,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js"
 const router = express.Router();
 
 router.route('/post').post(isAuthenticated,postJob);
-router.route('/get').get(isAuthenticated,getAllJob);
+router.route('/get').get(getAllJob);
 router.route('/getAdminjobs').get(isAuthenticated,getAdminJobs);
 router.route('/get/:id').get(isAuthenticated,getJobById);
 export default router;
